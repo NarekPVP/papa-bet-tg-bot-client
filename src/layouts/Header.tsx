f1f6a95f13ed0a5d5b6547d4ca6a4d2f2ac90946
NarekPVP <hnarek2005@gmail.com>
2024-06-19 16:07:06 +0400

    🎨 Added dark mode by default

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <h1 className="bg-gray-900 text-3xl font-bold underline text-white">Hello world!</h1>
 }
 
 export default Header
